cef560d3604fa80ac486c53a6dd947179dd606a5
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-04-25 17:32:26 +0800

    Incorporate chatGPT into flask app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, render_template
+import openai
 
 app = Flask(__name__)
+
+# Set your OpenAI API key as an environment variable
+API_KEY = "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"
+openai.api_key = API_KEY
+model_id = "gpt-3.5-turbo"
 
 # function to render HTML page
 @app.route('/')
@@ -11,18 +17,24 @@ def home():
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
     message = request.json['message']
+    print("Received message:", message)
 
-    # Define the chatbot's responses
-    if message.lower() in ['hi', 'hello']:
-        response = 'Hello there!'
-    elif message.lower() in ['how are you', 'how are you doing']:
-        response = "I'm doing well, thank you. How can I assist you?"
-    elif message.lower() in ['what is your name', 'what do you call yourself']:
-        response = "My name is ChatBot. I'm here to help you."
-    else:
-        response = "I'm sorry, I didn't understand what you said. Can you please rephrase that?"
+    # Define the chatbot's response
+    response = openai.ChatCompletion.create(
+        model=model_id,
+        messages=[{"role":"user",
+                   "content": message}]
+    )
+ 
+    # Extract the response text from the API response
+    response_text = response["choices"][0]["message"]["content"]
 
-    return jsonify({'message': response})
+    print(response_text)
+
+    # Return the response to the user
+    return jsonify({"message": response_text, "role": "assistant"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
