c9963f1094f4eddd66d0fb5fa2b04313cf6d2525
While-Luke <lukeellis3141@gmail.com>
2023-04-26 11:49:20 +0800

    added simple login page

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# function to render login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 # Initialize the conversation
 global_conversation = [
     {
